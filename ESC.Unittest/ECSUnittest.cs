31889915f74bb781b4dd1c18cb90c16a957e729a
Stonani <stefine.varde@gmail.com>
2020-02-16 15:11:29 +0100

    Lavet testprojeket, så nu kan vi skrive Unittest :) - Stefine

---
+++ ESC.Unittest/ECSUnittest.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using ECS.Redesign;
+
+namespace ESC.Unittest
+{
+    public class Tests
+    {
+        /// <summary>
+        /// Jeg har bare oprettet projektet til at kunne teste i :)
+        /// </summary>
+       
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
